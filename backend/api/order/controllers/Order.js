dc592dd9e9173414fa315e3e80817ca2a2be0600
Michael Cillo <mjc.rrnewtech@gmail.com>
2022-11-07 14:15:26 -0800

    Add files via upload

---
+++ backend/api/order/controllers/Order.js
@@ -0,0 +1,45 @@
+'use strict';
+/**
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
+ */
+// note that this needs to be a "private" key from STRIPE
+const stripe = require('stripe')(
+  'sk_test_51LrpVXLNmCdVbnyV2LkjPXP7BcBYjnuLaZeBpK0HzQBgTS5OgalIxfndkD9EucCBvCUC3AVhHcliFRfMbervK9G800bfTH9pwN'
+);
+module.exports = {
+  /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
+    const stripeAmount = Math.floor(amount * 100);
+    // charge on stripe
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: stripeAmount,
+      currency: 'usd',
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token,
+    });
+
+    // Register the order in the database
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
+      amount: stripeAmount,
+      address,
+      dishes,
+      city,
+      state,
+    });
+
+    return order;
+  },
+};
