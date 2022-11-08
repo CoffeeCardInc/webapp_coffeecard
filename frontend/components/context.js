645a823deeffb70ea8141aaa32b7d718645124ec
Michael Cillo <mjc.rrnewtech@gmail.com>
2022-11-07 17:06:08 -0800

    Add files via upload

---
+++ frontend/components/context.js
@@ -0,0 +1,17 @@
+/* /context/AppContext.js */
+
+import React from "react";
+// create auth context with default value
+
+// set backup default for isAuthenticated if none is provided in Provider
+const AppContext = React.createContext( 
+    {isAuthenticated:true, 
+        cart: {items:[], 
+        total:0}, 
+        addItem:()=>{}, 
+        removeItem:()=>{}, 
+        user:false, // default logged out
+        setUser:()=>{} 
+    });
+    
+export default AppContext;
