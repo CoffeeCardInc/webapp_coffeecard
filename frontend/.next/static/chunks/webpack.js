fd0a2e8ff08195c73c0d18864af159b19bd6e23b
andor <latandor@gmail.com>
2022-12-03 08:16:04 -0500

    merge

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1dda62ea828dc4f8"; }
+/******/ 		__webpack_require__.h = function() { return "2ed4437ed52a509b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
