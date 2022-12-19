4a552ecf2d0018f746c3183ba3b87dfc5b7518f1
andor <latandor@gmail.com>
2022-12-19 15:41:22 -0500

    new branch

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "51522de04a06c8eb"; }
+/******/ 		__webpack_require__.h = function() { return "a3b7cc195d36fc15"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
