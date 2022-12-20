8d70151702ee31b7c6f85e4494dd85aaa01a1640
andor <latandor@gmail.com>
2022-12-20 15:50:24 -0500

    postgres setup

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,12 +13,12 @@ import Landing from '../components/Landing'
 function Home() {
   // const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
   // console.log(`URL: ${API_URL}`)
-  const [query, setQuery] = useState('')
+  // const [query, setQuery] = useState('')
   // const link = new HttpLink({ uri: `${API_URL}/graphql` })
   // const cache = new InMemoryCache()
   // const client = new ApolloClient({ link, cache })
 
-  const [data, setData] = useState([])
+  // const [data, setData] = useState([])
 
   return (
     // <ApolloProvider client={client}>
@@ -37,7 +37,6 @@ function Home() {
           />
         </InputGroup> */}
       <Landing />
-      <br></br>
     </div>
 
     // </ApolloProvider>
