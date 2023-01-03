a7df9bfe7ed924a99a72c7ffeaac2b72fda2599e
andor <latandor@gmail.com>
2023-01-03 18:41:41 -0500

    user post request update

--- frontend/pages/api/coffeecard/users/index.js
+++ frontend/pages/api/coffeecard/users/index.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     res.status(200).json(allUsers)
   } else if (req.method == 'POST') {
     const { email, password, is_shop } = req.body
-    const result = await prisma.todo.create({
+    const result = await prisma.user.create({
       data: {
         email,
         password,
