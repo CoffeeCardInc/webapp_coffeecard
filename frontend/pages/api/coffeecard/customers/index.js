7426f1b05653f55fab19724777632695902b33cf
andor <latandor@gmail.com>
2023-01-05 07:55:34 -0500

    membership id endpoint with put and delete query setup

--- frontend/pages/api/coffeecard/customers/index.js
+++ frontend/pages/api/coffeecard/customers/index.js
@@ -2,15 +2,31 @@ import prisma from '../../../../lib/prisma'
 
 export default async function handler(req, res) {
   if (req.method == 'GET') {
-    const allUsers = await prisma.user.findMany()
+    const allUsers = await prisma.customer.findMany()
     res.status(200).json(allUsers)
   } else if (req.method == 'POST') {
-    const { email, password, is_shop } = req.body
-    const result = await prisma.user.create({
+    const {
+      full_name,
+      email,
+      is_shop,
+      passowrd,
+      date_of_birth,
+      language,
+      city,
+      membership_status,
+      user_id,
+    } = req.body
+    const result = await prisma.customer.create({
       data: {
+        full_name,
         email,
-        password,
         is_shop,
+        passowrd,
+        date_of_birth,
+        language,
+        city,
+        membership_status,
+        user_id,
       },
     })
     return res.json(result)
--- frontend/pages/api/coffeecard/member/[id].js
+++ frontend/pages/api/coffeecard/member/[id].js
@@ -2,16 +2,16 @@ import prisma from '../../../lib/prisma'
 
 export default async function handle(req, res) {
   if (req.method == 'PUT') {
-    // update a customer detail.
+    // update a change membership.
     const { id } = req.query
-    const post = await prisma.user.update({
+    const post = await prisma.membership.update({
       where: { id: Number(id) },
     })
     return res.json(post)
   } else if (req.method == 'DELETE') {
-    // delete a customer / account.
+    // delete a membership.
     const { id } = req.query
-    const post = await prisma.user.delete({
+    const post = await prisma.membership.delete({
       where: {
         id: Number(id),
       },
