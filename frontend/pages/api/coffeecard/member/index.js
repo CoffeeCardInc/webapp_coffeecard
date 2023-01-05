2261f61d8381fbf21a5c1a6843978ad5d791ddf8
andor <latandor@gmail.com>
2023-01-05 06:44:51 -0500

    prisma shop and pass relation setup done ont to many relation done

--- frontend/pages/api/coffeecard/member/index.js
+++ frontend/pages/api/coffeecard/member/index.js
@@ -1,6 +1,6 @@
 import prisma from '../../../../lib/prisma'
 
 export default async function handler(req, res) {
-  const allShops = await prisma.member.findMany()
-  res.status(200).json(allShops)
+  const allMember = await prisma.member.findMany()
+  res.status(200).json(allMember)
 }
