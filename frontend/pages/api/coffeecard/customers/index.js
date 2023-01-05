6ace33d22e6d415333c6a5f980e06483f0098107
andor <latandor@gmail.com>
2023-01-05 07:14:36 -0500

    customer and membership addon and edits

--- frontend/pages/api/coffeecard/customers/index.js
+++ frontend/pages/api/coffeecard/customers/index.js
@@ -1,6 +1,18 @@
 import prisma from '../../../../lib/prisma'
 
 export default async function handler(req, res) {
-  const allCustomer = await prisma.customer.findMany()
-  res.status(200).json(allCustomer)
+  if (req.method == 'GET') {
+    const allUsers = await prisma.user.findMany()
+    res.status(200).json(allUsers)
+  } else if (req.method == 'POST') {
+    const { email, password, is_shop } = req.body
+    const result = await prisma.user.create({
+      data: {
+        email,
+        password,
+        is_shop,
+      },
+    })
+    return res.json(result)
+  }
 }
