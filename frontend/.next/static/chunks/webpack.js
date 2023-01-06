2b600e7f924d4c80e06e4cfaa6c7eb64ddab1256
andor <latandor@gmail.com>
2023-01-06 12:58:23 -0500

    merging main and branch

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8847ecbba7060e7"; }
+/******/ 		__webpack_require__.h = function() { return "51bd9a6498ddb7de"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
