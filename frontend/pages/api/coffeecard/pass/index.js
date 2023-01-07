88ebc5c6a73c1cac7f86a238958e430c511fe0fb
Michael Cillo <mjc.rrnewtech@gmail.com>
2023-01-06 19:49:31 -0500

    moved all frontend/ contents onto root

--- frontend/pages/api/coffeecard/pass/index.js
+++
@@ -1,6 +0,0 @@
-import prisma from '../../../../lib/prisma'
-
-export default async function handler(req, res) {
-  const allPass = await prisma.pass.findMany({ include: { shop: true } })
-  res.status(200).json(allPass)
-}
