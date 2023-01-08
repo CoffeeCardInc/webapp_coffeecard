61e13b1b5ce5fdfb9304b8cd13b547bd5077ac80
Michael Cillo <mjcillo22@gmail.com>
2023-01-08 11:25:32 -0500

    added session provider from next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
 import { useContext, useState } from 'react'
 import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react"
+import { session } from 'next-auth'
+
 import Layout from '../components/layout'
 
-function MyApp(props) {
+export defailt function MyApp(props) {
   // const [state, setState] = useState({ cart: cart })
-  const { Component, pageProps } = props
+  const { Component, pageProps: {session, ...pageProps } } = props
 
   // setUser = (user) => {
   //   setState({ user })
   // }
 
   return (
-    <Layout className='container m-0 p-0' style={{ height: '100vh' }}>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout className='container m-0 p-0' style={{ height: '100vh' }}>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   )
 }
-
-export default MyApp
