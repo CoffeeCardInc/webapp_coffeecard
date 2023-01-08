b9c1407870595931f68dcbcd6dae5e15b86628a0
Michael Cillo <mjcillo22@gmail.com>
2023-01-08 12:02:15 -0500

    added email as provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import Providers from "next-auth/providers"
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,8 +9,19 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    Providers.Email({
+        server:{
+            host: "",
+            port: "",
+            auth: {
+                user: "",
+                pass: ""
+            }
+        }
+    })
     // ...add more providers here
   ],
+    
 }
 
 export default NextAuth(authOptions)
