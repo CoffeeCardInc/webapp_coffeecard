63a4b46defa98af21c0fa6a1c1ce4ad388146ad0
Michael Cillo <mjcillo22@gmail.com>
2023-01-08 12:25:55 -0500

    add docs in comment

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,8 @@ export default function MyApp({
   Component,
   pageProps: { session, pageProps}
 }) {
-
+  // session provides ability to pass the session all around
+  // [Documentation](https://next-auth.js.org/getting-started/client#sessionprovider)  
   return (
     <SessionProvider session={session}>
       <Layout className='container m-0 p-0' style={{ height: '100vh' }}>
