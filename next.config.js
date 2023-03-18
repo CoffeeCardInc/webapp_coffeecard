9fe8ce3c4b376d7b43cfa7489c462e7c87ff7559
andor <latandor@gmail.com>
2023-03-18 12:49:32 -0400

    sengrid change

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  /* config options here */
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false }
+
+    return config
+  },
 }
--- pages/contactus.js
+++ pages/contactus.js
@@ -1,24 +1,22 @@
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(
-  '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'
-)
+// const sgMail = require('@sendgrid/mail')
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const contactus = () => {
-  const msg = {
-    to: 'latandor@gmail.com', // Change to your recipient
-    from: 'test@example.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent')
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+  // const msg = {
+  //   to: 'latandor@gmail.com', // Change to your recipient
+  //   from: 'test@example.com', // Change to your verified sender
+  //   subject: 'Sending with SendGrid is Fun',
+  //   text: 'and easy to do anywhere, even with Node.js',
+  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  // }
+  // sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     console.log('Email sent')
+  //   })
+  //   .catch((error) => {
+  //     console.error(error)
+  //   })
 
   return (
     <>
