f7681244b7bc37e1b4020557498294f9e0f9c5f8
andor <latandor@gmail.com>
2023-03-24 18:08:05 -0400

    update

--- pages/me.jsx
+++ pages/me.jsx
@@ -1,29 +1,29 @@
-import { useSession, getSession } from "next-auth/react"
-import Layout from "../components/layout"
-import { unstable_getServerSession } from "next-auth"
-import { authOptions } from "pages/api/auth/[...nextauth]"
+import { useSession, getSession } from 'next-auth/react'
+import Layout from '../components/layout'
+import { getServerSession } from 'next-auth'
+import { authOptions } from 'pages/api/auth/[...nextauth]'
 
 export default function MePage() {
   const { data: session, status } = useSession({
     required: true,
     onUnauthenticated() {
-      return ("You must be signed in to view this page")
-    }
+      return 'You must be signed in to view this page'
+    },
   })
 
-  if (status === "loading") {
-  return "Loading or not authenticated..."
+  if (status === 'loading') {
+    return 'Loading or not authenticated...'
   }
 
   return <p>Signed in as {JSON.stringify(session)}</p>
 }
 // Replaced with useSession() hook
 export async function getServerSideProps(context) {
-  const session = await unstable_getServerSession(context.req, context.res, authOptions)
+  const session = await getServerSession(context.req, context.res, authOptions)
   if (!session) {
     return {
       redirect: {
-        destination: "/login",
+        destination: '/login',
         permanent: false,
       },
     }
